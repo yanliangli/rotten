7a42cfbc4455e33402a6982d94be11433778603b
yanliangli <z3200271@gmail.com>
2018-04-22 16:25:47 -0400

    update CelebrityRepository.java

--- cinema_paradiso/src/main/java/com/paridiso/cinema/persistence/CelebrityRepository.java
+++ cinema_paradiso/src/main/java/com/paridiso/cinema/persistence/CelebrityRepository.java
@@ -4,7 +4,13 @@ import com.paridiso.cinema.entity.Celebrity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CelebrityRepository extends JpaRepository<Celebrity, String>{
     Celebrity findCelebrityByCelebrityId(String celebrityId);
+
+    List<Celebrity> findCelebritiesByName(String keyword);
+
+    List<Celebrity> findCelebritiesByNameContains(String keyword);
 }
