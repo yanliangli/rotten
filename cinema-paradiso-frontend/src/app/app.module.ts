ca367f8fd0bc5c55969f8960c059d7b3ebb68939
yanliangli <z3200271@gmail.com>
2018-05-13 19:05:52 -0400

    Add files via upload

--- cinema-paradiso-frontend/src/app/app.module.ts
+++ cinema-paradiso-frontend/src/app/app.module.ts
@@ -6,6 +6,7 @@ import {AppComponent} from './app.component';
 import { CommonModule } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {NgxPaginationModule} from 'ngx-pagination'; // <-- import the module
+import { OrderModule } from 'ngx-order-pipe';
 
 import {HomeComponent} from './global/home/home.component';
 import {FooterComponent} from './global/footer/footer.component';
@@ -13,24 +14,38 @@ import {HeaderComponent} from './global/header/header.component';
 import {AppRoutingModule} from './app.routing';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {LoginComponent} from './global/login/login.component';
-import {CategoriesComponent} from './global/categories/categories.component';
 import {MovieDetailComponent} from './global/movie-detail/movie-detail.component';
 import {CelebrityComponent} from './global/celebrity/celebrity.component';
 import {RegUserComponent} from './user/reg-user/reg-user.component';
-import {SearchComponent} from './global/search/search.component';
 import {AdminComponent} from './user/admin/admin.component';
 import {LoginStatusService} from './global/login/login.status.service';
 import {AuthInterceptor} from './http.interceptor';
 import {HomeService} from './global/home/home.service';
 import {MovieDetailService} from './global/movie-detail/movie-detail.service';
 import {MovieService} from './global/movie/movie.service';
-import {CategoriesService} from './global/categories/categories.service';
 import {ToastrModule} from 'ngx-toastr';
-import { StarsComponent } from './global/stars/stars.component';
+import {StarsComponent} from './global/stars/stars.component';
 import {AboutComponent} from './global/general/about.component';
 import {ContactComponent} from './global/general/contact.component';
 import {PolicyComponent} from './global/general/policy.component';
-import {UserViewComponent} from './global/userView/userView.component';
+import {SidebarComponent} from './global/categories/sidebar/sidebar.component';
+import {OpeningThisWeekComponent} from './global/categories/opening_this_week/opening_this_week.component';
+import {ComingSoonComponent} from './global/categories/coming_soon/coming_soon.component';
+import {TopBoxOfficeComponent} from './global/categories/top_box_office/top_box_office.component';
+import {CertifiedFreshMovieComponent} from './global/categories/certified_fresh_movie/certified_fresh_movies.component';
+import {CertifiedFreshTVComponent} from './global/categories/certified_fresh_tv/certified_fresh_tv.component';
+import {MostPopularTVComponent} from './global/categories/most_popular_tv/most_popular_tv.component';
+import {NewTVTonightComponent} from './global/categories/new_tv_tonight/new_tv_tonight.component';
+import {TopRatedTVComponent} from './global/categories/top_rated_tv/top_rated_tv.component';
+import {AcademyAwardComponent} from './global/categories/acadamy_award/academy_award.component';
+import {TopRatedMovieComponent} from './global/categories/top_rated_movies/top_rated_movie.component';
+import {SearchComponent} from './global/search/search.component';
+import {SearchToolbarComponent} from './global/search/search-toolbar/search-toolbar.component';
+import {SearchMovieTabComponent} from './global/search/movie-tab/movie-tab.component';
+import {SearchTVTabComponent} from './global/search/tv-tab/tv-tab.component';
+import {SearchPeopleTabComponent} from './global/search/people-tab/people-tab.component';
+import {SearchService} from './global/search/search.service';
+
 @NgModule({
   // entryComponents: [MessageComponent],
   declarations: [
@@ -39,17 +54,30 @@ import {UserViewComponent} from './global/userView/userView.component';
     FooterComponent,
     HeaderComponent,
     LoginComponent,
-    CategoriesComponent,
     MovieDetailComponent,
     CelebrityComponent,
     RegUserComponent,
-    UserViewComponent,
     SearchComponent,
+    SearchToolbarComponent,
+    SearchMovieTabComponent,
+    SearchPeopleTabComponent,
+    SearchTVTabComponent,
     AdminComponent,
     StarsComponent,
     AboutComponent,
     ContactComponent,
     PolicyComponent,
+    SidebarComponent,
+    OpeningThisWeekComponent,
+    ComingSoonComponent,
+    TopBoxOfficeComponent,
+    CertifiedFreshMovieComponent,
+    CertifiedFreshTVComponent,
+    MostPopularTVComponent,
+    NewTVTonightComponent,
+    TopRatedTVComponent,
+    TopRatedMovieComponent,
+    AcademyAwardComponent,
   ],
   imports: [
     BrowserModule,
@@ -61,13 +89,14 @@ import {UserViewComponent} from './global/userView/userView.component';
     BrowserAnimationsModule,
     ToastrModule.forRoot(),
     NgxPaginationModule,
+    OrderModule,
   ],
   providers: [
     LoginStatusService,
     HomeService,
     MovieService,
     MovieDetailService,
-    CategoriesService,
+    SearchService,
     {
     provide: HTTP_INTERCEPTORS,
     useClass: AuthInterceptor,
--- cinema-paradiso-frontend/src/app/app.routing.ts
+++ cinema-paradiso-frontend/src/app/app.routing.ts
@@ -1,7 +1,6 @@
-import {Router, RouterModule, Routes} from '@angular/router';
+import {Router, RouterModule, Routes, UrlSegment} from '@angular/router';
 import {HomeComponent} from './global/home/home.component';
 import {NgModule} from '@angular/core';
-import {CategoriesComponent} from './global/categories/categories.component';
 import {MovieDetailComponent} from './global/movie-detail/movie-detail.component';
 import {CelebrityComponent} from './global/celebrity/celebrity.component';
 import {RegUserComponent} from './user/reg-user/reg-user.component';
@@ -10,19 +9,43 @@ import {AdminComponent} from './user/admin/admin.component';
 import {StarsComponent} from './global/stars/stars.component';
 import {ContactComponent} from './global/general/contact.component';
 import {AboutComponent} from './global/general/about.component';
-import {UserViewComponent} from './global/userView/userView.component';
+import {OpeningThisWeekComponent} from './global/categories/opening_this_week/opening_this_week.component';
+import {ComingSoonComponent} from './global/categories/coming_soon/coming_soon.component';
+import {TopBoxOfficeComponent} from './global/categories/top_box_office/top_box_office.component';
+import {CertifiedFreshMovieComponent} from './global/categories/certified_fresh_movie/certified_fresh_movies.component';
+import {CertifiedFreshTVComponent} from './global/categories/certified_fresh_tv/certified_fresh_tv.component';
+import {MostPopularTVComponent} from './global/categories/most_popular_tv/most_popular_tv.component';
+import {NewTVTonightComponent} from './global/categories/new_tv_tonight/new_tv_tonight.component';
+import {TopRatedTVComponent} from './global/categories/top_rated_tv/top_rated_tv.component';
+import {TopRatedMovieComponent} from './global/categories/top_rated_movies/top_rated_movie.component';
+import {AcademyAwardComponent} from './global/categories/acadamy_award/academy_award.component';
+import {SearchMovieTabComponent} from './global/search/movie-tab/movie-tab.component';
+import {SearchTVTabComponent} from './global/search/tv-tab/tv-tab.component';
+import {SearchPeopleTabComponent} from './global/search/people-tab/people-tab.component';
 
 const appRoutes: Routes = [
-  // {path: '', pathMatch: 'full', redirectTo: 'home'},
+  //{path: '', pathMatch: 'full', redirectTo: 'home'},
   {path: 'home', component: HomeComponent},
-  {path: 'movies', component: CategoriesComponent},
+  //{path: 'movies', component: OpeningThisWeekComponent},
+  {path: 'movies/opening_this_week',component: OpeningThisWeekComponent},
+  {path: 'movies/coming_soon',component: ComingSoonComponent},
+  {path: 'movies/top_box_office',component: TopBoxOfficeComponent},
+  {path: 'movies/certified_fresh_movie',component: CertifiedFreshMovieComponent},
+  {path: 'movies/new_tv_tonight',component: NewTVTonightComponent},
+  {path: 'movies/most_popular_tv',component: MostPopularTVComponent},
+  {path: 'movies/certified_fresh_tv',component: CertifiedFreshTVComponent},
+  {path: 'movies/academy_award',component: AcademyAwardComponent},
+  {path: 'movies/top_rated_movie',component: TopRatedMovieComponent},
+  {path: 'movies/top_rated_tv',component: TopRatedTVComponent},
   {path: 'celebrity/:id', component: CelebrityComponent},
   {path: 'search', component: SearchComponent},
+  {path: 'search/movie', component: SearchMovieTabComponent},
+  {path: 'search/tv', component: SearchTVTabComponent},
+  {path: 'search/celebrity', component: SearchPeopleTabComponent},
   {path: 'admin', component: AdminComponent},
   {path: 'star', component: StarsComponent},
   {path: 'movie/:id', component: MovieDetailComponent},
   {path: 'user/:id', component: RegUserComponent},
-  {path: 'userView/:id', component: UserViewComponent},
   {path: 'home/:id', component: MovieDetailComponent},
   {path: 'about', component: AboutComponent},
   {path: 'contact', component: ContactComponent},
@@ -32,5 +55,7 @@ const appRoutes: Routes = [
   imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule {
+export class AppRoutingModule{
+
 }
+
