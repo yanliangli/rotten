2a1ed73d0296d4974449bf8f0471c4956740a51f
yanliangli <z3200271@gmail.com>
2018-05-14 16:30:01 -0400

    search with filter

--- cinema_paradiso/src/main/java/com/paridiso/cinema/controller/SearchController.java
+++ cinema_paradiso/src/main/java/com/paridiso/cinema/controller/SearchController.java
@@ -4,6 +4,7 @@ import com.paridiso.cinema.entity.Celebrity;
 import com.paridiso.cinema.entity.Movie;
 import com.paridiso.cinema.entity.TV;
 import com.paridiso.cinema.service.SearchService;
+import com.paridiso.cinema.service.implementation.MovieServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -30,25 +31,28 @@ public class SearchController {
     @Autowired
     SearchService searchService;
 
+    @Autowired
+    MovieServiceImpl filmService;
+
     @RequestMapping(value = "/movie", method = GET)
-    public ResponseEntity<Page> searchMovie(@RequestParam("keyword") String keyword, @RequestParam(value = "page", defaultValue = "0") Integer page,@RequestParam(value = "limit", defaultValue = "7") Integer size) {
-        Sort sort = new Sort(Sort.Direction.DESC, "rating");
+    public ResponseEntity<Page> searchMovie(@RequestParam("keyword") String keyword, @RequestParam(value = "page", defaultValue = "0") Integer page,@RequestParam(value = "itemsPerPage", defaultValue = "7") Integer size, @RequestParam(value = "sortBy", defaultValue = "rating") String sortBy, @RequestParam(value = "order", defaultValue = "DESC") String order) {
+        Sort sort = filmService.getSortParam(order, sortBy);
         Pageable pageable = new PageRequest(page, size, sort);
         Page<Movie> moviePage = searchService.getMoviesFromKeyword(keyword, pageable);
         return new ResponseEntity<>(moviePage, HttpStatus.OK);
     }
 
     @RequestMapping(value = "/celebrity", method = GET)
-    public ResponseEntity<Page> searchCelebrity(@RequestParam("keyword") String keyword, @RequestParam(value = "page", defaultValue = "0") Integer page,@RequestParam(value = "limit", defaultValue = "7") Integer size) {
-        Sort sort = new Sort(Sort.Direction.DESC, "name");
+    public ResponseEntity<Page> searchCelebrity(@RequestParam("keyword") String keyword, @RequestParam(value = "page", defaultValue = "0") Integer page,@RequestParam(value = "itemsPerPage", defaultValue = "7") Integer size, @RequestParam(value = "sortBy", defaultValue = "name") String sortBy, @RequestParam(value = "order", defaultValue = "ASC") String order) {
+        Sort sort = filmService.getSortParam(order, sortBy);
         Pageable pageable = new PageRequest(page, size, sort);
         Page<Celebrity> celebrityPage = searchService.getCelebritiesFromKeyword(keyword, pageable);
         return new ResponseEntity<>(celebrityPage, HttpStatus.OK);
     }
 
     @RequestMapping(value = "/tv", method = GET)
-    public ResponseEntity<Page> searchTV(@RequestParam("keyword") String keyword, @RequestParam(value = "page", defaultValue = "0") Integer page,@RequestParam(value = "limit", defaultValue = "7") Integer size) {
-        Sort sort = new Sort(Sort.Direction.DESC, "rating");
+    public ResponseEntity<Page> searchTV(@RequestParam("keyword") String keyword, @RequestParam(value = "page", defaultValue = "0") Integer page,@RequestParam(value = "itemsPerPage", defaultValue = "7") Integer size, @RequestParam(value = "sortBy", defaultValue = "rating") String sortBy, @RequestParam(value = "order", defaultValue = "DESC") String order) {
+        Sort sort = filmService.getSortParam(order, sortBy);
         Pageable pageable = new PageRequest(page, size, sort);
         Page<TV> tvPage = searchService.getTVsFromKeyword(keyword, pageable);
         return new ResponseEntity<>(tvPage, HttpStatus.OK);
